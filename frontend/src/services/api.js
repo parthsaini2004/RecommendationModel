6f10c2d88202e97b07887ec83bdf6227aaba9dc6
parthsaini2004 <129434562+parthsaini2004@users.noreply.github.com>
2024-12-22 12:16:25 +0530

    base url changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,14 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:5000/api', // URL of the Node.js server
+// });
+
+// export default api;
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // URL of the Node.js server
+  baseURL: 'https://recommendationmodelbackend.onrender.com/api', // Deployed backend URL
 });
 
 export default api;
